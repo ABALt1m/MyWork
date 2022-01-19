3a431a7f9b52d5cccae8744ec0b68bf1c511b943
ABALt1m <302587881@student.rocmondriaan.nl>
2022-01-19 18:08:48 +0100

    1

---
+++ actul portfolio/java/main.js
@@ -0,0 +1,14 @@
+function toggle() {
+    var popup1 = document.getElementById('popup1')
+    popup1.classList.toggle('active');
+}
+
+function toggle2() {
+    var popup2 = document.getElementById('popup2')
+    popup2.classList.toggle('active');
+}
+
+function toggle3() {
+    var popup3 = document.getElementById('popup3')
+    popup3.classList.toggle('active');
+}
