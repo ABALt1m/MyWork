da2dc75b5afb48ef13a3adcd607a1806b61ed924
ABALt1m <302587881@student.rocmondriaan.nl>
2022-03-09 07:24:21 +0100

    carousel done

--- javascccript/carrousel/main.js
+++ javascccript/carrousel/main.js
@@ -1,38 +1,16 @@
-let i = 0;
-let images = [];
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
-images[0] = 'img1.jpg'
-images[1] = 'img2.jpg'
-images[2] = 'img3.jpg'
-images[3] = 'img4.jpg'
-images[4] = 'img5.jpg'
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
+        
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
 
-function changeImg(){
-    document.slide.src = images[i];
-
-    if(i < images.length - 1){
-        i++
-    }
-    else{
-        i = 0
-    }
-}
-
-function previousImg(){
-    document.slide.src = images[i];
-
-    if(i < images.length - 1){
-        i--
-    }
-    else{
-        i = 0
-    }
-}
-
-let next = document.querySelector('.nxt')
-let previous = document.querySelector('.prev')
-
-next.addEventListener('click', changeImg)
-previous.addEventListener('click', previousImg)
-
-window.onload = changeImg;
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
